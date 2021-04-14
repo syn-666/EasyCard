7b231e6a33f9af48767129d20e67c4a134af2790
syn <1599386309@qq.com>
2021-04-14 16:10:52 +0800

    init

---
+++ EasyCard/EasyCard/EasyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyCardApp.swift
+//  EasyCard
+//
+//  Created by 史雅楠 on 2021/4/14.
+//
+
+import SwiftUI
+
+@main
+struct EasyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
